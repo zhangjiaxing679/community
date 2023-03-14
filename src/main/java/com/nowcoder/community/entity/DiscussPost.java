9216396db0c71def843c765ee55d52d291a2b052
zhangjiaxing <zhangjiaxing@nowcoder.com>
2023-03-14 22:58:26 +0800

    第二次提交

--- src/main/java/com/nowcoder/community/entity/DiscussPost.java
+++ src/main/java/com/nowcoder/community/entity/DiscussPost.java
@@ -1,16 +1,49 @@
 package com.nowcoder.community.entity;
 
+//import co.elastic.clients.elasticsearch._types.mapping.FieldType;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.deser.std.StringDeserializer;
+import com.fasterxml.jackson.databind.ser.std.StringSerializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
 import java.util.Date;
 
+@JsonIgnoreProperties(ignoreUnknown=true)
+@Document(indexName = "discusspost")
 public class DiscussPost {
+    @Id
     private int id;
+
+    @Field(type= FieldType.Integer)
     private int userId;
+
+    @Field(type = FieldType.Text,analyzer = "ik_max_word",searchAnalyzer = "ik_smart")
+    @JsonSerialize(using= StringSerializer.class)
+    @JsonDeserialize(using = StringDeserializer.class)
     private String title;
+
+    @Field(type = FieldType.Text,analyzer = "ik_max_word",searchAnalyzer = "ik_smart")
     private String content;
+
+    @Field(type =FieldType.Integer)
     private int type;
+
+    @Field(type = FieldType.Integer)
     private int status;
+
+    @Field(type = FieldType.Date)
     private Date createTime;
+
+    @Field(type = FieldType.Integer)
     private int commentCount;
+
+    @Field(type=FieldType.Double)
     private double score;
 
 
