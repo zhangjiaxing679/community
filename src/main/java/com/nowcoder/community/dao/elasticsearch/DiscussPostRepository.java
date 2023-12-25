860e2b11b210ff96bb921bcdb42e95095325f21d
zhangjiaxing <zhangjiaxing@nowcoder.com>
2023-12-25 17:18:29 +0800

    暂时屏蔽搜索功能

--- src/main/java/com/nowcoder/community/dao/elasticsearch/DiscussPostRepository.java
+++ src/main/java/com/nowcoder/community/dao/elasticsearch/DiscussPostRepository.java
@@ -1,37 +1,37 @@
-package com.nowcoder.community.dao.elasticsearch;
-
-import com.nowcoder.community.entity.DiscussPost;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.elasticsearch.annotations.Highlight;
-import org.springframework.data.elasticsearch.annotations.HighlightField;
-import org.springframework.data.elasticsearch.annotations.HighlightParameters;
-import org.springframework.data.elasticsearch.core.SearchHit;
-import org.springframework.data.elasticsearch.core.SearchHits;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.List;
-
-@Repository
-public interface DiscussPostRepository extends ElasticsearchRepository<DiscussPost, Integer> {
-
-    @Highlight(
-            fields = {
-                    @HighlightField(name = "title"),
-                    @HighlightField(name="content")
-            }
-//            parameters = @HighlightParameters(
-//                    preTags = "<strong><font style='color:red'>",
-//                    postTags = "</font></strong>",
-//                    fragmentSize = 500,
-//                    numberOfFragments = 3
-//            )
-    )
-    List<SearchHit<DiscussPost>> findByTitleOrContent(String title,String content,Pageable pageable);
-//    SearchHit<DiscussPost> findByTitleOrContent(String title, String content, Pageable pageable);
-
-}
-
-
+//package com.nowcoder.community.dao.elasticsearch;
+//
+//import com.nowcoder.community.entity.DiscussPost;
+//import org.springframework.data.domain.Page;
+//import org.springframework.data.domain.Pageable;
+//import org.springframework.data.domain.Sort;
+//import org.springframework.data.elasticsearch.annotations.Highlight;
+//import org.springframework.data.elasticsearch.annotations.HighlightField;
+//import org.springframework.data.elasticsearch.annotations.HighlightParameters;
+//import org.springframework.data.elasticsearch.core.SearchHit;
+//import org.springframework.data.elasticsearch.core.SearchHits;
+//import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+//import org.springframework.stereotype.Repository;
+//
+//import java.util.List;
+//
+//@Repository
+//public interface DiscussPostRepository extends ElasticsearchRepository<DiscussPost, Integer> {
+//
+//    @Highlight(
+//            fields = {
+//                    @HighlightField(name = "title"),
+//                    @HighlightField(name="content")
+//            }
+////            parameters = @HighlightParameters(
+////                    preTags = "<strong><font style='color:red'>",
+////                    postTags = "</font></strong>",
+////                    fragmentSize = 500,
+////                    numberOfFragments = 3
+////            )
+//    )
+//    List<SearchHit<DiscussPost>> findByTitleOrContent(String title,String content,Pageable pageable);
+////    SearchHit<DiscussPost> findByTitleOrContent(String title, String content, Pageable pageable);
+//
+//}
+//
+//
